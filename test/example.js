a30c8b334bff7b8e7eac118576e29d62b2d929c9
Daniel Diaz <diazjfdaniel@gmail.com>
2015-07-27 23:44:43 -0700

    Added mocha and makefile for unit testing purposes

---
+++ test/example.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('example', function() {
+    describe('sub-example', function() {
+        it('should return true', function() {
+            assert.equal(1, 1);
+        });
+    });
+});
