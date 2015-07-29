ec61258af0dbca15ef23e967fb5fcc8b2fd737e9
Daniel Diaz <diazjfdaniel@gmail.com>
2015-07-28 22:20:46 -0700

    CI

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 var port = process.env.PORT || 8080;
 
 app.set('port', port);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(app.get('port'), function() {
     console.log('Server started on localhost: ' + app.get('port'));
